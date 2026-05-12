25171e70d2a0f322ef9ca498a39536a2084309de
youngrapc <youngrapc@gmail.com>
2026-05-12 18:29:33 +0900

    feat: restore page.js with safe code

---
+++ app/page.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Home() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>열무 출퇴근 시스템</h1>
+      <p>배포에 성공했습니다! 이제 기능을 추가해볼까요?</p>
+    </div>
+  )
+}
